d7fc079becf5094b76e0ce16b696f7b61af048c2
Yu, Jonathan Kwangsik <jonathan.yu@gatech.edu>
2025-01-22 17:15:28 -0600

    modify init script

--- redis-init.sh
+++ redis-init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# start the Redis server in the background
+redis-server --daemonize yes
+
+# wait for Redis server to start
+sleep 5
+
 QUOTES_FILE="/docker-entrypoint-initdb.d/quotes.json"
 
 # import quotes from the quotes.json file into Redis
@@ -11,3 +17,6 @@ while IFS= read -r line; do
 done < <(jq -c '.[]' "$QUOTES_FILE")
 
 echo "Quotes have been successfully imported into Redis."
+
+# keep the container running
+tail -f /dev/null
