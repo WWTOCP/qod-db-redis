6c90de9e049cbae7030c2dc2bac06cd9d946222a
Yu, Jonathan Kwangsik <jonathan.yu@gatech.edu>
2025-01-22 18:52:18 -0600

    fix json parsing

--- redis-init.sh
+++ redis-init.sh
@@ -9,16 +9,15 @@ sleep 5
 QUOTES_FILE="/docker-entrypoint-initdb.d/quotes.json"
 
 # import quotes from the quotes.json file into Redis
-index=1
 while IFS= read -r line; do
-  # parse json fields using jq
-  id=$(echo "$line" | jq -r '.id')
+  # parse JSON fields using jq
+  id=$(printf "%s" "$(echo "$line" | jq -r '.id')")
   text=$(echo "$line" | jq -r '.text')
   author=$(echo "$line" | jq -r '.author')
   genre=$(echo "$line" | jq -r '.genre')
 
+  # store the quote in Redis as a hash
   redis-cli HMSET "quote:$id" id "$id" text "$text" author "$author" genre "$genre"
-  ((index++))
 done < <(jq -c '.[]' "$QUOTES_FILE")
 
 echo "Quotes have been successfully imported into Redis."
